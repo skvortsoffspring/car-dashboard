97934b29c9a3f17395319fb0161e0cafaf0e0e31
Igor <garikmogilev@gmail.com>
2024-10-27 21:42:41 +0100

    measure real voltage with resistance divider

--- arduino/acum/acum.ino
+++ arduino/acum/acum.ino
@@ -3,17 +3,24 @@
 #include "acum.h"
 #include <common.h>
 
+#define TEXT_UNIT_SIZE 3
+#define OFFSE_UNIT_PX_Y 170
 #define UNIT "volt"
+
+#define VREF 5.0  // value of your stabilizer voltage
+#define R1 10000  // measure the resistance and make adjustments
+#define R2 4700   // measure the resistance and make adjustments
+#define ADC 1024
 
 Adafruit_GC9A01A tft(TFT_CS, TFT_DC);
 
 void setup() {
   init(&tft);
   drawSymbol(&tft, logo);
-  drawUnit(&tft, UNIT, 3, 170);
+  drawUnit(&tft, UNIT, TEXT_UNIT_SIZE, OFFSE_UNIT_PX_Y);
   drawBorder(&tft);
 }
 
 void loop() {
-  sensorReadings(&tft, 14.4);
+  sensorReadings(&tft, analogRead(A0) * VREF * ((R1 + R2) / R2) / ADC);
 }
